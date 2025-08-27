346d5b46bc0791838782f2de9f9724e36ec04954
Ranbeer Singh <tridev06052009@gmail.com>
2025-08-27 15:37:18 +0000

    [LeetCode Sync] Runtime - 9 ms (49.91%), Memory - 45 MB (12.00%)

---
+++ 0455-assign-cookies/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        int n=g.size(), m=s.size();
+        sort(g.begin(), g.end()); sort(s.begin(), s.end());
+        int l=0,r=0;
+        while(l<m && r<n){
+            if(s[l]>=g[r]) r=r+1;
+            l=l+1;
+        }
+        return r;
+    }
+};
