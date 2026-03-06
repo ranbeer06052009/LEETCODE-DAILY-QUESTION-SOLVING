fd13ac4675f80442fbe54eabb2cb5d12962c2054
Ranbeer Singh <tridev06052009@gmail.com>
2026-03-06 17:36:56 +0000

    [LeetCode Sync] Runtime - 7 ms (9.49%), Memory - 17.8 MB (66.42%)

--- 2032-largest-odd-number-in-string/solution.cpp
+++ 2032-largest-odd-number-in-string/solution.cpp
@@ -1,23 +1,11 @@
 class Solution {
 public:
     string largestOddNumber(string num) {
-        string ans="";
-        int ind;
-        bool flag=false;
-        for(int i=num.size()-1; i>=0; i--){
-            if(num[i]%2!=0){
-                ind=i;
-                flag=true;
-                break;
+        for (int i = num.length() - 1; i >= 0; i--) {
+            if ((num[i] - '0') % 2 == 1) {
+                return num.substr(0, i + 1);
             }
         }
-        if(!flag) return ans;
-        for(int i=0; i<=ind;i++) {
-            if(num[0]=='0'&& i==0);
-            else{
-                ans.push_back(num[i]);
-            }
-        }
-        return ans;
+        return "";        
     }
 };
