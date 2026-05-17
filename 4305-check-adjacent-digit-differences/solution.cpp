15de35921a8775b7c2f0dee87ee0c1edf913cc53
Ranbeer Singh <tridev06052009@gmail.com>
2026-05-17 02:38:26 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.7 MB (47.22%)

---
+++ 4305-check-adjacent-digit-differences/solution.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool isAdjacentDiffAtMostTwo(string s) {
+        int n=s.size();
+        for(int i=0; i<n-1; i++){
+            if(abs(s[i]-s[i+1])>2)return false;
+        }
+        return true;
+    }
+};
