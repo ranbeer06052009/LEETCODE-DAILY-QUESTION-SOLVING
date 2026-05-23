b2d0f1818b375821d037c9304749070bc37ff510
Ranbeer Singh <tridev06052009@gmail.com>
2026-05-23 09:34:52 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 11.3 MB (54.85%)

--- 1878-check-if-array-is-sorted-and-rotated/solution.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/solution.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
-         int n = nums.size();
-        int count = 0;
-        
-        // Check if the array is non-decreasing
-        for (int i = 1; i < n; i++)
-            if (nums[i - 1] > nums[i])
-                count++;
-        
-        // Check if the last element is greater than the first element
-        if (nums[n - 1] > nums[0])
-            count++;
-        
-        // If the count of violations is less than or equal to 1, return true
-        return count <= 1;
-       
+        int n = nums.size();
+        if (n <= 1) return true;
+        int inversionCount = 0;
+        for (int i = 1; i < n; ++i) {
+            if (nums[i] < nums[i - 1]) {
+                inversionCount++;
+                if (inversionCount > 1) return false;
+            }
+        }
+        if (nums[0] < nums[n - 1]) inversionCount++;
+        return inversionCount <= 1;
     }
 };
