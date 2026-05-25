ac29b85955032a6a6ed7b00fc9b6e0020d8d03b7
Ranbeer Singh <tridev06052009@gmail.com>
2026-05-25 12:01:05 +0000

    [LeetCode Sync] Runtime - 11 ms (20.64%), Memory - 19.9 MB (38.60%)

---
+++ 0207-course-schedule/solution.cpp
@@ -0,0 +1,37 @@
+class Solution {
+private:
+    vector<int> topoSort(int &V, vector<vector<int>> adj){
+        int indegree[V];
+        for(int i=0; i<V; i++)indegree[i]=0;
+        for(int i=0; i<V; i++){
+            for(auto it:adj[i]){
+                indegree[it]++;
+            }
+        }
+        queue<int> q;
+        for(int i=0; i<V; i++){
+            if(indegree[i]==0)q.push(i);
+        }
+        vector<int> topo;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            topo.push_back(node);
+            for(auto it:adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0)q.push(it);
+            }
+        }
+        return topo;
+    }
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+        for(int i=0; i<prerequisites.size(); i++){
+            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+        }
+        vector<int> topo=topoSort(numCourses,adj);
+        if(topo.size()==numCourses)return true;
+        return false;
+    }
+};
