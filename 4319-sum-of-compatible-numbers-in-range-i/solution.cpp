0b75cca0bcae9c71ac725c5ae768f32e3bbd9f5c
Ranbeer Singh <tridev06052009@gmail.com>
2026-06-07 03:29:18 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.5 MB (98.45%)

---
+++ 4319-sum-of-compatible-numbers-in-range-i/solution.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int sumOfGoodIntegers(int n, int k) {
+        int ans=0;
+        for(int i=n-k; i<=n+k; i++){
+            if(abs(n-i)<=k&&(n&i)==0)ans+=(i>0)?i:0;
+        }
+        return ans;
+    }
+};
