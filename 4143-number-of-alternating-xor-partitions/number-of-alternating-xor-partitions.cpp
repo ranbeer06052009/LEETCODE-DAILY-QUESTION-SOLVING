795364531e6a40fe66e084550c0773c93b66fd8d
Ranbeer Singh <tridev06052009@gmail.com>
2026-07-16 21:38:31 +0530

    Time: 35 ms (81.38%) | Memory: 138.6 MB (90.34%) - LeetSync

---
+++ 4143-number-of-alternating-xor-partitions/number-of-alternating-xor-partitions.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int alternatingXOR(vector<int>& nums, int target1, int target2) {
+        const int MOD = 1000000007;
+        int n = nums.size();
+        int prefix = 0;
+        vector<int> state = {0, target1,  target1^target2, target2};
+        vector<int> dp(4);
+        vector<int> pre_dp(4);
+        pre_dp[0] = 1;
+        dp[0] = 1;
+        for(int i=0; i<n; i++){
+            prefix ^= nums[i];
+            if(i == n-1){
+                int total = 0;
+                for(int j=0; j<4; j++){
+                    if(prefix == state[j])
+                        total =  (total + pre_dp[(j+3)%4])%MOD;
+                }
+                return total;
+            }
+            for(int j=0; j<4; j++){
+                if(prefix == state[j])
+                    dp[j] = (dp[j] + pre_dp[(j+3)%4])%MOD;
+            }
+            pre_dp = dp;
+        }
+       
+        return 0;
+    }
+};
