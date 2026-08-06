5860b74b697fa98b8599e2b1b47f51081429e928
Ranbeer Singh <tridev06052009@gmail.com>
2026-08-06 06:34:00 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (97.62%) - LeetSync

---
+++ 3626-smallest-divisible-digit-product-i/smallest-divisible-digit-product-i.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int smallestNumber(int n, int t) {
+        int q=n/10, r=n%10;
+        int req = t / gcd(max(q, 1), t);
+        int nxt;
+
+        if(r%req ==0)
+            nxt=r;
+        else
+            nxt=(r/req+1) * req;
+
+        if (nxt < 10)
+            return q * 10 + nxt;
+
+        return (q + 1) * 10;
+    }
+};
